21a25f073578bbb7b39454d8f9710c5d1ff37e7a
amine-zzr <aminezoukri@gmail.com>
2024-03-08 02:01:26 +0000

    saving my work for later

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+'''
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    '''
+    def __init__(self, *args, **kwargs):
+        '''
+        '''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def save(self):
+        '''
+        '''
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''
+        '''
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        '''
+        '''
+        obj_dict = self.__dict__.copy()
+        obj_dict['__calss__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        return obj_dict
