b82ccff6cce2f5172afdd7689ab90123c214c5f1
amine-zzr <aminezoukri@gmail.com>
2024-03-13 02:34:21 +0000

    testing instantiation of the FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,23 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
+
+    def test_FileStorage_instantiation_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_FileStorage_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testFileStorage_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+
 class TestFileStorage(unittest.TestCase):
     """Unit tests for the FileStorage class."""
 
